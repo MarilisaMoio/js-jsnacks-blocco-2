0be7be22b90ef19be8399a984c965371520b1fac
MarilisaMoio <moiomarilisa@gmail.com>
2024-03-12 16:48:21 +0100

    feat: add snack-1

--- snack-1/js/script.js
+++ snack-1/js/script.js
@@ -1,1 +1,12 @@
 //L'utente inserisce un numero nel prompt, se è pari stampa il numero, se è dispari stampa il numero successivo
+
+//chiediamo il numero
+let userNumber = parseInt(prompt("Inserisci un numero"));
+
+//incrementiamo se dispari
+if (userNumber % 2 !== 0){
+    userNumber++
+}
+
+//output
+console.log(userNumber)
