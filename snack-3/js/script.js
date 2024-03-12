b701e265689c43809195671c67aec635c715f5d6
MarilisaMoio <moiomarilisa@gmail.com>
2024-03-12 18:10:57 +0100

    feat: add snack-3

--- snack-3/js/script.js
+++ snack-3/js/script.js
@@ -1,6 +1,5 @@
 //Crea due tag div con due id diversi: un div avrà il testo colorato di rosso mentre l’altro di verde. Partendo da un array di numeri, stampiamo nell’id rosso i numeri dispari e in verde i numeri pari.
 
-
 //creazione dei div
 const newDivEven = document.createElement("div");
 newDivEven.id = "even";
@@ -9,17 +8,35 @@ const newDivOdd = document.createElement("div");
 newDivOdd.id = "odd";
 
 //array di partenza
-let array = [1, 2, 3, 4];
+const startingNumbers = [];
 
-//verifica delle condizioni e relativo spostamento in div
-for (let i = 0; i < array.length; i++){
-    if (array[i] % 2 === 0){
-        newDivEven.append(array[i])
-    } else (
-        newDivOdd.append(array[i])
-    )
+const arrayLength = 10;
+
+for (let i = 1; i <= arrayLength; i++){
+    const randomNumber = Math.floor(Math.random() * 100);
+
+    startingNumbers.push(randomNumber);
 }
 
+const numbersEven = [];
+
+const numbersOdd = [];
+
+//verifica delle condizioni e relativo spostamento nell'array corretto
+for (let i = 0; i < startingNumbers.length; i++){
+    if (startingNumbers[i] % 2 === 0){
+        numbersEven.push(startingNumbers[i]);
+    } else {
+        numbersOdd.push(startingNumbers[i]);
+    }
+}
+
+//aggiunta al relativo div con spazi annessi
+newDivEven.innerHTML = numbersEven.join(" ");
+
+newDivOdd.innerHTML = numbersOdd.join(" ");
+
 //aggiunta al doc
-document.body.append(newDivEven)
-document.body.append(newDivOdd)
+document.body.append(newDivEven);
+
+document.body.append(newDivOdd);
