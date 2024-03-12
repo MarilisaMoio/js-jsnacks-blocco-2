ff50c3f0326e04df134f44ca5641069bbd2e97a7
MarilisaMoio <moiomarilisa@gmail.com>
2024-03-12 16:58:53 +0100

    feat: add snack-2

--- snack-2/js/script.js
+++ snack-2/js/script.js
@@ -1,1 +1,12 @@
 //L’utente inserisce due parole in successione, con due prompt. Il software stampa prima la parola più corta, poi la parola più lunga.
+
+//acquisici le due parole
+const firstWord = prompt("Inserisci una parola");
+const secondWord = prompt("Inserisci un'altra parola");
+
+//compara la lunghezza e printa di conseguenza
+if (firstWord.length > secondWord.length){
+    console.log(secondWord, firstWord)
+} else { //vale anche per pari lunghezza
+    console.log(firstWord, secondWord)
+}
